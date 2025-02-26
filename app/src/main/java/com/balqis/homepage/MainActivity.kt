6454bd82eb1550bee5d7fce15d662e643b8b5560
balqissph <balqissph@gmail.com>
2025-02-26 08:10:59 +0700

    penambahan logtag untuk email

--- app/src/main/java/com/balqis/homepage/MainActivity.kt
+++ app/src/main/java/com/balqis/homepage/MainActivity.kt
@@ -1,20 +1,51 @@
 package com.balqis.homepage
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        supportActionBar?.hide()
+
+        val editTextEmail = findViewById<EditText>(R.id.editTextTextEmailAddress)
+        val editTextPassword = findViewById<EditText>(R.id.editTextTextPassword)
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+
+        buttonLogin.setOnClickListener {
+            val emailInput = editTextEmail.text.toString().trim()
+            val password = editTextPassword.text.toString().trim()
+
+            if (emailInput.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Email dan Password harus diisi!", Toast.LENGTH_SHORT).show()
+            } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+                Toast.makeText(this, "Format email tidak valid!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Berhasil!", Toast.LENGTH_SHORT).show()
+
+                val logTag = "Email"
+
+                if (logTag == "Email") {
+                    Log.d(logTag, emailInput)
+                } else {
+                    Log.d(logTag, "")
+                }
+
+                val intent = Intent(this, HomeActivity::class.java)
+                intent.putExtra("email", emailInput)
+                startActivity(intent)
+
+                finish()
+            }
         }
     }
 }
